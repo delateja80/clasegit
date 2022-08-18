4181f7c5d523861c96b684a92360a9801ee04099
Julio Alberto de la Teja López <jdelatejal242@profesor.uemex.mx>
2022-08-17 22:01:30 -0500

    tercero commit

--- src/Ejemplo.java
+++ src/Ejemplo.java
@@ -2,5 +2,6 @@ public class Ejemplo {
     public static void main(String[] args) {
         System.out.println("hola mundo");
         System.out.println("desde java");
+        System.out.println("otro");
     }
 }
